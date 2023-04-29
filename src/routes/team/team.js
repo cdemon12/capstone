c75b0a084b8bbca5b444a0956ab90c23a66252b3
cdemon12 <coleschnell@gmail.com>
2023-04-29 17:04:07 -0500

    update bios

--- src/routes/team/team.js
+++ src/routes/team/team.js
@@ -3,7 +3,7 @@ export default [
       "slug": "brooke",
       "name": "Brooke Muckerman",
       "photo": "https://github.com/cdemon12/capstone/raw/main/src/routes/team/photos/brooke.jpg",
-      "bio": "I am a graduate from the University of Missouri Journalism School, where I studied print and digital journalism as well as political science. As a reporter for the Columbia Missourian, I was a City Government Reporter, Government Impact Reporter and a State Government Reporter. Outside of journalism, I am a member of Kappa Delta Social Sorority. In my free time I enjoy swimming and spending time outside.",
+      "bio": "Brooke is a graduate from the University of Missouri Journalism School, where she studied print and digital journalism as well as political science. As a reporter for the Columbia Missourian, she was a City Government Reporter, Government Impact Reporter and a State Government Reporter. Outside of journalism, Brooke is a member of Kappa Delta Social Sorority. In her free time she enjoys swimming and spending time outside.",
       "title": "Reporter",
       "socials": [{icon: 'logo-twitter', link:'https://twitter.com/BrookeMuckerman'},{icon: 'logo-linkedin', link:'https://www.linkedin.com/in/brookemuckerman/edit/self-id/controls/?profileFormEntryPoint=POST_APPLY'},]
     },
@@ -11,7 +11,7 @@ export default [
       "slug": "rylee",
       "name": "Rylee Fels",
       "photo": "https://github.com/cdemon12/capstone/raw/main/src/routes/team/photos/rylee.jpg",
-      "bio": "I am originally from Carlsbad, CA. While at The University of Missouri, I studied broadcast journalism and minored in business. I am passionate about using storytelling to create engaging and informative content. Whether it's through writing, filming, or editing, I am dedicated to bringing stories to life in a creative and compelling way.",
+      "bio": "Rylee is originally from Carlsbad, CA. During her time at The University of Missouri, she studied broadcast journalism and minored in business. Rylee is passionate about using storytelling to create engaging and informative content. Whether it's through writing, filming, or editing, she is dedicated to bringing stories to life in a creative and compelling way.",
       "title": "Video Editor",
       "socials": [{icon: 'logo-twitter', link:'https://twitter.com/ryleefelstv'},{icon: 'logo-linkedin', link:'https://www.linkedin.com/in/rylee-fels-668a2a167/'},]
     },
@@ -21,13 +21,13 @@ export default [
       "photo": "https://github.com/cdemon12/capstone/raw/main/src/routes/team/photos/cela.jpg",
       "bio": "Cela holds a degree in Journalism with an emphasis in social and audience strategy, as well as a minor in sociology. She has social media management experience with Inside Climate News, Vox Magazine and NBC News.",
       "title": "Reporter, Social Media Coordinator",
-      "socials": [{icon: 'logo-linkedin', link:'https://www.linkedin.com/in/celamigan'},{icon: 'unknown', link:'https://j4502-ss22.github.io/migan-portfolio-site/index.html, https://gondola.cc/celamigan'},]
+      "socials": [{icon: 'logo-linkedin', link:'https://www.linkedin.com/in/celamigan'},{icon: 'person-circle-outline', link:'https://j4502-ss22.github.io/migan-portfolio-site/index.html'}, {icon:'desktop-outline', link:'https://gondola.cc/celamigan'},]
     },
     {
       "slug": "kristin",
       "name": "Kristin Kuchno",
       "photo": "https://github.com/cdemon12/capstone/raw/main/src/routes/team/photos/kristin.jpg",
-      "bio": "Kristin Kuchno is a journalist with experience in feature writing, breaking news reporting and copy editing. A St. Louis native, she graduated from the University of Missouri in 2023 with a bachelor’s degree in journalism. She previously wrote for the Columbia Missourian newspaper where she was the assistant city editor of the community beat.",
+      "bio": "Kristin is a journalist with experience in feature writing, breaking news reporting and copy editing. A St. Louis native, she graduated from the University of Missouri in 2023 with a bachelor’s degree in journalism. She previously wrote for the Columbia Missourian newspaper where she was the assistant city editor of the community beat.",
       "title": "Reporter",
       "socials": [{icon: 'logo-twitter', link:'https://twitter.com/kuchnokristin?s=21&t=u-Y24UNHx1IX8bAP8_LB_w'},{icon: 'logo-linkedin', link:'https://www.linkedin.com/in/kristin-kuchno'},]
     },
