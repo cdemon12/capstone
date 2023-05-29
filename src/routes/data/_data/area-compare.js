30ff539da1177d009c7644bc97c96497e81e781b
cdemon12 <coleschnell@gmail.com>
2023-05-29 14:40:59 -0500

    delete unused files

--- src/routes/data/_data/area-compare.js
+++
@@ -1,5 +0,0 @@
-export default {
-  "race": ["asian", "black", "indian", "white"],
-  "nominal": [0.0212, 0.3557, 0.0188, 0.6043],
-  "per_capita": [0.0626, 0.5534, 0.1913, 0.1928]
-}
--- src/routes/data/stores.ts
+++
@@ -1,5 +0,0 @@
-import colorScheme from "./_data/colorScheme";
-import { writable } from "svelte/store";
-
-
-export const colors = writable(colorScheme.grays);
