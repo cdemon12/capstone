3a6cbe2d3e6412d7c4ea8ceed4baccb7cedadcc8
cdemon12 <coleschnell@gmail.com>
2023-06-01 18:28:00 -0500

    fix header link

--- src/routes/_components/Detour.svelte
+++ src/routes/_components/Detour.svelte
@@ -29,7 +29,7 @@
 <div class="top-bar">
 <div class="progress-bar" style="width:{scrollprogress}%"></div>
 <nav>
-    <div class="logo-container"><a href="/"><img src="https://github.com/cdemon12/capstone/raw/main/src/routes/_assets/detour.png" alt="Detour logo"></a></div>
+    <div class="logo-container"><a href="https://capstone-rosy.vercel.app/"><img src="https://github.com/cdemon12/capstone/raw/main/src/routes/_assets/detour.png" alt="Detour logo"></a></div>
     <div class="socials">
         <ul>
             <li><a href="https://www.facebook.com/sharer/sharer.php?u={main_url + slug}"><ion-icon name="logo-facebook"></ion-icon></a></li>
